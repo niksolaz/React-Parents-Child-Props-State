3da6b6cd018d3a7d0f514258f6e70c3e496205f9
niksolaz <solazzo.nicola@gmail.com>
2015-10-18 16:09:17 +0200

    basic index

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-var React = require('react');
+/*
+Create basic template Main.js 
+*/
+//Call dependencies
+var React = require('react');          
 var ReactDOM = require('react-dom');
 
 ReactDOM.render(
-  <h1>Welcome on the tutorial React!</h1>,
-  document.getElementById('example')
+  <h1>Welcome on the tutorial React!</h1>, // this appear in index.html from <div>
+  document.getElementById('example') // 'example' is id from <div> in index.html
 );
