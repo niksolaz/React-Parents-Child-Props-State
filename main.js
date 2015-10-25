7cc9dd6cb2421b8cf93c5cc74035255039f7fd39
niksolaz <solazzo.nicola@gmail.com>
2015-10-25 15:13:27 +0100

    add comment in main.js

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 "use strict";
-
+/*
+	- CommentBox
+        - CommentList
+          - Comment
+        - CommentForm
+*/
 var CommentBox = React.createClass({
   
   render: function() {
     return (
 		<div className="commentBox">
 			<h1>Comments</h1>
+			//add component CommentList and CommentBox
 			<CommentList />
 			<CommentForm />
 		</div>    	
@@ -19,6 +25,7 @@ var CommentList = React.createClass({
 		return (
 			<div className="commentList">
 				CommentList
+				//add component Comment and call propieties author
 				<Comment author="John Doe">This is one comment</Comment>
 				<Comment author="Mario Rossi">This is *another* comment</Comment>
 			</div>
@@ -38,6 +45,7 @@ var CommentForm = React.createClass({
 });
 
 var Comment = React.createClass({
+	//protect from an XSS attack
 	rawMarkup: function() {
 		var rawMarkup = marked(this.props.children.toString(),{sanitize:true});
 		return { __html:rawMarkup };
@@ -46,13 +54,17 @@ var Comment = React.createClass({
 		return (
 			<div className="comment">
 				<h2 className="commentAuthor">
+					//using propieties author
 					{this.props.author}
 				</h2>
+				//use markup
 				<span dangerouslySetInnerHTML={this.rawMarkup()} />
 			</div>
 		);
 	}
 });
+
+//rederize Main Component "CommentBox"
 ReactDOM.render(
 	<CommentBox />, 
 	document.getElementById("myContent")
