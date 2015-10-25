f34c5607b22a611348aed2d5fb230fca51bb3311
niksolaz <solazzo.nicola@gmail.com>
2015-10-25 12:15:30 +0100

    create first component for structure comment list following tutorial react

--- main.js
+++ main.js
@@ -1,128 +1,17 @@
 "use strict";
 
-var WelcomeMessage = React.createClass({
+var CommentBox = React.createClass({
   
   render: function() {
     return (
-	    	<div className="welcomeMessage">
-	    		<section>
-		    	<h1>
-		    	Welcome on the tutorial React by Nik Solaz
-		    	</h1>    	
-			    	<li><PropsMessage/></li>
-			    	<li><StatesMessage /></li>
-			    	<li><ParentsMessage /></li>
-			    	<li><ChildrensMessage /></li>
-			    <CommentList />
-			    </section>
-			    <section>
-			    	<MainList />
-			    </section>
-	    	</div>
-    	 );
-  }
-});
-
-var PropsMessage = React.createClass({
-
-	render: function(){
-		return (
-			<div className="propsMessage">
-				Props
-			</div>
-		);
-	}
-});
-
-var StatesMessage = React.createClass({
-
-	render: function(){
-		return (
-			<div className="statesMessage">
-				State
-			</div>
-		);
-	}
-});
-
-var ParentsMessage = React.createClass({
-
-	render: function(){
-		return (
-			<div className="parentsMessage">
-				Parents
-			</div>
-		);
-	}
-});
-
-var ChildrensMessage = React.createClass({
-
-	render: function(){
-		return (
-			<div className="childrensMessage">
-				Childrens
-			</div>
-		);
-	}
-});
-
-var Comment = React.createClass({
-  render: function() {
-    return (
-      <div className="comment">
-        <h2 className="commentAuthor">
-          {this.props.author}
-        </h2>
-        {this.props.children}
-      </div>
-
+		<div className="commentBox">
+			This is a Comment Box
+		</div>    	
     );
   }
-});
-
-var CommentList = React.createClass({
-  render: function() {
-    return (
-      <section className="commentList">
-        <Comment author="Props Author">This is one comment using "this.props.author"</Comment>
-        <p>The author is a value property. We have associated in this case the value Props, how could we give Author any other name</p>
-        <Comment author="Props Children">This is *another* comment using "this.props.children"</Comment>
-        <p>With children recall the "author's name" with another name</p>
-      </section>
-    );
-  }
-});
-
-var MainList =  React.createClass({
-	render: function(){
-		return (
-			<section className="mainList">
-				
-					<UnderList food="Coffee"/>
-					<UnderList food="Tea"/>
-					<UnderList food="Milk"/>
-				
-			</section>
-		);
-	}
-});
-
-var UnderList =  React.createClass({
-	render: function(){
-		return (
-			<div className="underList">
-				
-				  <p className="commentFood">{this.props.food}</p>
-				  {this.props.children}
-				  {this.props.children}
-				
-			</div>
-		);
-	}
 });
 
 ReactDOM.render(
-	<WelcomeMessage />, 
-	document.getElementById("myMessageID")
+	<CommentBox />, 
+	document.getElementById("myContent")
 	);
